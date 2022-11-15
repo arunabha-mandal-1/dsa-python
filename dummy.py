5aace0844ef1e7485b7789cf496062c05896ce2e
Arunabha Mandal <arunabhamandal461@gmail.com>
2022-11-15 15:59:00 +0530

    dummy.py updated

--- dummy.py
+++ dummy.py
@@ -1,4 +1,10 @@
-a=None
-b=None
-print(id(a), id(b))
-print(a is not b)
+# a=None
+# b=None
+# print(id(a), id(b))
+# print(a is not b)
+
+a=[[], [], []]
+a[0].append((1,2))
+a[0].append((1,3))
+a[1].append((4,3))
+print(a)
