01818807d353fa843697299e2b6b83012a3a2578
Arunabha Mandal <arunabhamandal461@gmail.com>
2022-11-17 22:40:26 +0530

    dummy.py modified

--- dummy.py
+++ dummy.py
@@ -3,8 +3,12 @@
 # print(id(a), id(b))
 # print(a is not b)
 
-a=[[], [], []]
-a[0].append((1,2))
-a[0].append((1,3))
-a[1].append((4,3))
-print(a)
+# a=[[], [], []]
+# a[0].append((1,2))
+# a[0].append((1,3))
+# a[1].append((4,3))
+# print(a)
+
+# a=[[],[],[]]
+# a[0][0]=(2,3) # wil not work
+# print(a)
